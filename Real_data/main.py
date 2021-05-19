1c4b121f75001e586d7e452c5df3c2df1db7c9c2
Morris Kim <morris.kim@binus.ac.id>
2021-05-20 03:45:45 +0700

    re-ordered files, added bar graph functionality

--- Real_data/main.py
+++ Real_data/main.py
@@ -4,66 +4,133 @@ import numpy as np
 import pandas as pd
 import scipy as sp
 from scipy import stats
-from scipy.stats import norm
 import matplotlib.pyplot as plt
 
-max = 0
-while(max<=0):
-    max = int(input("How many people do you want to send the survey to: "))
-    if(max<=0):
-        print("You must input a number greater than 0!")
-x = np.random.randint(1, max)  # randomized sample size
-anime_num_list = np.random.randint(200, 576, size=10000) #changed to this from 1-1001 due to it generating a large std and an unrealistic mean
+#import dataset
+file_name = "users_cleaned.csv"
+file_dir = os.path.join(sys.path[0], file_name)
+data = pd.read_csv(file_dir, delimiter = ',')
+columns = list(data.columns)
 
-print(f"The survey is sent to {max} people")
-print(f"This is the number of people who answered: {x}")
+#initialize variable
+quit = False
 
-mean = np.mean(anime_num_list)
-std = np.std(anime_num_list)
+#menu functions
+def get_column(purpose, frequency_enabled = False):
+    while True:
+        column = input("Enter which column "+ purpose +": ")
+        if column not in columns:
+            if frequency_enabled and column == "frequency":
+                break
+            print("That is not a valid column!")
+        else:
+            break
+    return column
 
-new_data = sp.stats.norm(mean, std).rvs(x) #generates float (try find a way to make it genereate int instead if possible)
-domain = np.linspace(np.min(new_data), np.max(new_data))
+def get_graph_type():
+    while True:
+        print("============GRAPH MENU============")
+        print("1. Histogram")
+        print("2. Bar Graph")
+        print("3. Scatter plot")
+        print("4. Back")
+        choice = int(input("Enter your choice(1-4): ")) #don't forget to do a check for data type (rn it crashes if you input string)
+        if choice < 1 or choice > 4:
+            print("That is not a valid choice!")
+        else:
+            break
+    return choice
 
-mean = np.mean(new_data)
-std = np.std(new_data)
-median = np.median(new_data)
-mode = stats.mode(new_data).mode[0]
-var = np.var(new_data)
+def visualize_hist(column, data):
+    column_data = data[column]
+    if(type(column_data[0]) == str):
+        print("You cannot create a histogram from this column!")
+    else:
+        bins = int(input("How many bins do you want: "))
+        n, bins, patches = plt.hist(column_data, bins=bins, edgecolor='black') #histogram for the total list answer
+        plt.xlabel(column)
+        plt.ylabel("Frequency")
+        plt.show()
 
+def visualize_bar(x, y, data):
+    #show just the first 1000 unique values
+    over_limit = False
+    uniques = data[x].unique().tolist()
+    if len(uniques) >= 100: #too many uniques
+        print("There are too many unique values! X-axis shortened to 100 values to reduce crashes")
+        over_limit = True
+    if y == "frequency":
+        frequency_count = data[x].value_counts()[:101] if over_limit else data[x].value_counts()
+        uniques = frequency_count.index.tolist()
+        plt.bar(uniques, frequency_count)
+        plt.xlabel(x)
+        plt.ylabel("Frequency")
+        plt.show()
+    else:
+        if type(data[y]) == str:
+            print("You cannot create a bar graph using non-numeric y-values")
+        else:
+            #how do you deal with duplicate x-values e.g. 2 people from india having a different number of animes_completed
+            if(len(uniques) != len(data)):
+                print("There are duplicate values that may conflict with each other")
+            else:
+                temp_data = data.sort_values(by=[y], ascending=False)
+                y_values = temp_data[y][:101] if over_limit else temp_data[y]
+                x_values = temp_data[x][:101] if over_limit else temp_data[x]
+                plt.bar(x_values, y_values)
+                plt.xlabel(x)
+                plt.ylabel(y)
+                plt.show()
 
-print(f"Mean = {mean}")
-print(f"Median = {median}")
-print(f"Mode = {mode}") #because new data consists of float, mode is most likely float
-print(f"Standard deviation = {std}")
-print(f"Variance = {var}")
-print(f"Skew = {stats.skew(new_data)}")
+def visualize_scatter(x, y, data):
+    pass
 
-
-n, bins, patches = plt.hist(new_data, bins= 20, edgecolor='red') #histogram for the total list answer
-plt.xlabel("Number of anime")
-plt.ylabel("Frequency")
-plt.title("Anime")
-plt.show()
-
+while not quit:
+    print("============MAIN MENU============")
+    print("Column names: ", end="")
+    for column in columns[:-1]:
+        print(column, end=", ")
+    print(columns[-1])
+    print("1. Describe column")
+    print("2. Generate Graph")
+    print("3. Compare new value")
+    print("4. Quit")
+    while True:
+        choice = int(input("Enter your choice here: "))
+        if choice < 1 or choice > 4:
+            print("That is not a valid choice!")
+        else:
+            break
+    if choice == 1:
+        column = get_column("to describe")
+        print("============DESCRIPTION============")
+        description = data[column].describe()
+        print(description, end="\n\n")
+    elif choice == 2:
+        graph_type = get_graph_type()
+        if graph_type == 1: #hist
+            column = get_column("to make into a histogram")
+            visualize_hist(column, data)
+        elif graph_type == 2: #bar
+            x = get_column("to be used as the x-axis")
+            y = get_column("to be used as the y-axis(you can write 'frequency')", True)
+            visualize_bar(x, y, data)
+        elif graph_type == 3: #scatter
+            x = get_column("to be used as the x-axis")
+            y = get_column("to be used as the y-axis")
+            visualize_scatter(x, y, data)
+    elif choice == 3:
+        pass
+    else:
+        quit = True
 
 
 #inferential statistics
-# print(stats.zscore(anime_num_list))
-# plt.hist(stats.zscore(anime_num_list), bins = 20)
-anime_num = int(input("How many anime that you watch: "))
-zscore = (anime_num - mean)/std
-print(zscore)
-
-plt.plot(domain, norm.pdf(domain, mean, std), label='$\mathcal{N}$ ' + f'$( \mu \\approx {round(mean)} , \sigma \\approx {round(std)} )$')
-n, bins, patches = plt.hist(new_data, bins= 20, edgecolor='red', density = True) #histogram for the total list answer
-plt.xlabel("Number of anime")
-plt.ylabel("Frequency")
-plt.title("Density Histogram")
-plt.legend()
-plt.show()
-
-
-
-# anime_time = int(input("how many list for the time: "))
-
-
+# anime_num = int(input("How many anime that you watch: "))
+# zscore = (anime_num - mean)/std
+# print(zscore)
+    
+#check data type of column
+# column_data = data[column]
+# if string bar graph
+# if numbers histogram
